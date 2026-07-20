c7cffabca449635f580d7f358ca2df2dfe21d3a2
ushatkaalam <ushashok@gmail.com>
2026-07-19 20:43:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+//
+// Wait until HTML has loaded
+//
+document.addEventListener("DOMContentLoaded", function ()
+{
 
+    //
+    // Locate the button
+    //
+    const btnUserManual =
+        document.getElementById("btnUserManual");
+
+    //
+    // Attach click event
+    //
+    btnUserManual.addEventListener(
+        "click",
+        showUserManual
+    );
+
+});
+
+
+//
+// Display User Manual
+//
+function showUserManual()
+{
+    const viewer =
+        document.getElementById("pdfViewer");
+
+    viewer.src = "docs/User_Manual.pdf";
+}
